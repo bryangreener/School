197c7e4b31c18aaac2bbbdf31e23becf0fbb802e
BryanGreener <bryancgreener@gmail.com>
2017-11-02 11:35:56 -0400

    Add Assignment5 pdf

---
+++ WebTech/JS Notes.js
@@ -0,0 +1,29 @@
+JavaScript Notes
+
+Example conditional:
+	var x;
+	var y = 0;
+	y = 4;
+	x = (y==4) ? "y is 4" : "y is not 4"; //String is saved to x;
+
+
+Example Function:
+	power(2,10);
+	function power(x,y)
+	{
+		//code in here
+	}
+
+Array:
+	var arr = new Array();
+
+	var arr = new Array("item1", "item2");
+
+	var arr = ["item1", "item2"];
+	arr.push("item3"); // Append item to array
+	arr.pop(); // Remove from end of array
+
+Strings:
+	var str = "test";
+	var count = str.length;
+

5f7362de6e8d85725f252d79c1cb1df110043327
Bryan Greener <bryancgreener@gmail.com>
2018-01-21 21:51:37 -0500

    Add homework.py. Need to test

---
+++ Spring 2018/Python/Assignments/Homework 1.py
@@ -0,0 +1,34 @@
+# Name: Bryan Greener
+# Date: 2018-01-21
+# Homework: 1
+
+# PART 1
+string1 = input("Enter string 1:")
+string2 = input("Enter string 2:")
+if string1 in string2:
+    print("True")
+else: 
+    print("False")
+
+# PART 2
+pal = input("Enter string:")
+revPal = pal[::-1]
+if pal == revPal:
+    print("True")
+else:
+    print("False")
+
+# PART 3
+num1 = input("Enter number 1:")
+num2 = input("Enter number 2:")
+print (num1 + num2)
+
+# PART 4
+bnum = abs(input("Enter number:"))
+print bnum[2:]
+
+# PART 5
+string1 = input("Enter string 1:")
+string2 = input("Enter string 2:")
+print (string1 in string2)
+

05335e8da368fd442cd63fdb5b74bcbdee47a126
Bryan Greener <bryancgreener@gmail.com>
2018-01-22 17:55:30 -0500

    Add class notes and update python homework

--- Spring 2018/Python/Assignments/Homework 1.py
+++ Spring 2018/Python/Assignments/Homework 1.py
@@ -19,16 +19,16 @@ else:
     print("False")
 
 # PART 3
-num1 = input("Enter number 1:")
-num2 = input("Enter number 2:")
-print (num1 + num2)
+num1 = int(input("Enter number 1:"))
+num2 = int(input("Enter number 2:"))
+print(num1 + num2)
 
 # PART 4
-bnum = abs(input("Enter number:"))
-print bnum[2:]
+bnum = abs(int(input("Enter number:")))
+print("{0:b}".format(bnum))
 
 # PART 5
 string1 = input("Enter string 1:")
 string2 = input("Enter string 2:")
-print (string1 in string2)
+print(string1 in string2)
 

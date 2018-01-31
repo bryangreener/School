52669906b460d84b24c8b935bc786550aac5f423
Bryan Greener <bryancgreener@gmail.com>
2018-01-31 09:34:37 -0500

    Update unique word counter again after clarification in class

--- Spring 2018/Python/Assignments/Assignment#2/hw#2_Greener.py
+++ Spring 2018/Python/Assignments/Assignment#2/hw#2_Greener.py
@@ -57,6 +57,8 @@ for key, value in wd.items():
                 least[y] = least[y-1]
             least[i] = key
             break
+    if value == 1:
+        unique += 1
 # Print out results
 print("-----------------------------------")
 print("# Most Frequent Words:")
@@ -71,4 +73,4 @@ print("___________________________________")
 for x in range(0, 10):
     print(least[x].ljust(20), wd.get(least[x]))
 print("-----------------------------------")
-print("Number of Unique Words:\n%s" % len(wd))
+print("Number of Unique Words:\n%s" % unique)

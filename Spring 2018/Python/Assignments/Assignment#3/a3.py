e880749fb3577f77e65f782da978a96c1b3f7cd1
Bryan Greener <bryancgreener@gmail.com>
2018-02-13 22:55:23 -0500

    Nearly completed with a3

--- Spring 2018/Python/Assignments/Assignment#3/a3.py
+++ Spring 2018/Python/Assignments/Assignment#3/a3.py
@@ -1,12 +1,21 @@
 def check_hex(user_input):
     try:
+        user_input = int(user_input, 16)
         user_input = hex(user_input)
         return True
     except ValueError:
         return False
-def hex_operation():
-    return True
-def hex_binary():
+def hex_operation(input_list, operation):
+    result = input_list[0]
+    for i in range(1,len(input_list)):
+        if(operation == 1):
+            result |= input_list[i]
+        elif(operation == 2):
+            result &= input_list[i]
+        else:
+            result ^= input_list[i]
+    return result
+def hex_binary(input_list, operation):
     return True
 operation = 0
 num_integers = 0
@@ -33,15 +42,20 @@ while(num_integers < 2):
     except ValueError:
         print("Please enter a number greater than 1")
 
-input_dict = {}
+input_list = []
 valid_hex = False
-for i in range(1,num_integers+1):
+for i in range(0,num_integers):
     user_input = input("Enter integer %d:" % i)
     valid_hex = check_hex(user_input)
     while(valid_hex == False):
         print("Please enter an 8-digit hexadecimal integer")
         user_input = input("Enter integer %d:" % i)
         valid_hex = check_hex(user_input)
-        
-    input_dict = {i: user_input}
-    print(input_dict[i])
+    hex_in = int(user_input, 16)
+    input_list.append(hex_in)
+    print(i, input_list[i])
+result = hex_operation(input_list, operation)
+# Hex formatted output
+print("%08x".upper() % result)
+result = hex_operation(input_list, operation)
+print("{0:032b}".format(result))

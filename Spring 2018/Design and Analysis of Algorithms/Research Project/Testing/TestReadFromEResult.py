7a7217495e734a9fa46614c69beaad4adf5edb1f
Bryan Greener <bryancgreener@gmail.com>
2018-04-05 17:44:23 -0400

    Complete testing of wide range of inputs

--- Spring 2018/Design and Analysis of Algorithms/Research Project/Testing/TestReadFromEResult.py
+++ Spring 2018/Design and Analysis of Algorithms/Research Project/Testing/TestReadFromEResult.py
@@ -1,7 +1,7 @@
 import ast
 import matplotlib.pyplot as plt
 import numpy as np
-file = open('NewSigmoidTest.txt', 'r')
+file = open('NNOutput2018-04-05.txt', 'r')
 mainlist = []
 for line in file:
     mainlist.append(line)
@@ -9,14 +9,27 @@ temp = []
 for i in mainlist:
     temp.append(ast.literal_eval(i))
 it = 0
-learnrates=[0.5,1.5,2.5]
-for i in temp[0][0]:
-    fig = plt.figure()    
-    for j in i:
-        plt.plot(np.array(j[:30]))
-    plt.title("Learn Rate {0}".format(learnrates[it]))
-    plt.legend([15,30,50], loc='lower right')
-    plt.grid(1)
-    plt.show()
-    it += 1
+layerlist=['784-10-10','784-10-10-10','784-10-10-10-10',
+             '784-15-10','784-15-15-10','784-15-15-15-10',
+             '784-20-10','784-20-20-10','784-20-20-20-10',
+             '784-25-10','784-25-25-10','784-25-25-25-10',
+             '784-30-10','784-30-30-10','784-30-30-30-10',
+             '784-50-10','784-50-50-10','784-50-50-50-10']
+learnrates=['1.0','2.0','3.0','4.0','5.0','6.0','7.0','8.0','9.0','10.0']
+
+for i in range(len(layerlist)):
+    for j in temp[i][0]:
+        fig = plt.figure()    
+        for k in j:
+            plt.plot(np.array(k[:30]))
+        plt.title("Design: {0} -- Learn Rate: {1}".format(layerlist[i], learnrates[it]))
+        plt.legend(['5', '10', '15', '20', '25', '30', '35', '40', '45', '50'], loc='lower right')
+        plt.grid(1)
+        plt.show()
+        fig.savefig("./Graphs/{0}_{1}.png".format(layerlist[i], learnrates[it]))
+        plt.close(fig)
+        it += 1
+    it = 0
+        
+        
 

ee59a39cbbf3479da265e2070b8331f0030db738
bryangreener <bryancgreener@gmail.com>
2018-04-17 13:53:55 -0400

    Start work on 6p2

---
+++ Spring 2018/Python/Assignments/Assignment#6/hw#6PartII_BryanGreener.py
@@ -0,0 +1,37 @@
+def lev(a, len_a, b, len_b):
+    @MemoizeReset
+    cost = 0
+    if len_a == 0: return len_b
+    if len_b == 0: return len_a
+    if a[len_a - 1] == b[len_b - 1]:
+        cost = 0
+    else:
+        cost = 1
+    
+    return min(lev(a, len_a - 1, b, len_b) + 1,
+               lev(a, len_a, b, len_b - 1) + 1,
+               lev(a, len_a - 1, b, len_b - 1) + cost)
+    
+class MemoizeReset(object):
+    def __init__(self, f):
+        self.f = f
+        self.cache = {}
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.f(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+            return self.f(*args)
+    def __repr__(self):
+        return self.f.__doc__
+    def __get__(self, obj, objtype):
+        fn = functools.partial(self.__call__, obj)
+        fn.reset = self._reset
+        return fn
+    def _reset(self):
+        self.cache = {}
+    
+

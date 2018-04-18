bb078a14d2e3825dbf784eaf2a6c549376025344
Bryan Greener <bryancgreener@gmail.com>
2018-04-17 21:27:47 -0400

    Refer to previous commit message

--- Spring 2018/Python/Assignments/Assignment#6/hw#6PartII_BryanGreener.py
+++ Spring 2018/Python/Assignments/Assignment#6/hw#6PartII_BryanGreener.py
@@ -2,19 +2,21 @@
 # Date: 2018-04-17
 # Homework: 6 part 2
 
+import sys
+
 def MemoizeReset(f):
-    mem = {}
-    def MR(*args, **kwargs):
-        MR.calls += 1
-        key = str(args) + str(kwargs)
-        if key not in mem:
-            mem[key] = f(*args, **kwargs)
-        return mem[key]
-    MR.calls = 0
+    cache = {} # Dict acting as cache
+    def MR(*args, **kwargs): # Main helper func. Accumulates count and caches
+        val = str(args) + str(kwargs)
+        if val not in cache:
+            MemoizeReset.count += 1
+            cache[val] = f(*args, **kwargs)
+        return cache[val]
+    MR.count = 0
     MR.__name__ = f.__name__
     return MR
 
-@MemoizeReset
+@MemoizeReset # Decorator for func above
 def lev(a, b):
     if a == "":
         return len(b)
@@ -24,15 +26,28 @@ def lev(a, b):
         cost = 0
     else:
         cost = 1
-    res = min([lev(a[:-1], b)+1,
-               lev(a, b[:-1])+1,
-               lev(a[:-1], b[:-1]) + cost])
-    return res
+    output = min([lev(a[:-1], b)+1,
+                  lev(a, b[:-1])+1,
+                  lev(a[:-1], b[:-1]) + cost])
+    return output
 
 import csv
-with open('wordfile.txt', 'r') as file:
-    reader = csv.reader(file, delimiter=',')
-    for row in reader:
-        print("%s,%s,%d,%s" % 
-              (row[0].strip(), row[1].strip(), 
-               lev(row[0].strip(),row[1].strip()),str(lev.calls)))
+if sys.argv[1] == "-f" and len(sys.argv) > 2:
+    filename = sys.argv[2]
+else:
+    print("No file specified. Exiting")
+    exit()
+try:
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            print("%s,%s,%d,%s" % (row[0].strip(), 
+                                   row[1].strip(), 
+                                   lev(row[0].strip(),row[1].strip()),
+                                   str(lev.count)))
+except OSError as err:
+    print("OS Error: {0}".format(err))
+    exit()
+except NameError as err:
+    print("Type Error: {0}".format(err))
+    exit()

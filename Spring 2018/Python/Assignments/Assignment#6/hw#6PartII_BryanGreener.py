6264643be320155828d6bfda18e2baf3863b44cb
bryangreener <bryancgreener@gmail.com>
2018-04-18 09:54:25 -0400

    Finish assignment 6

--- Spring 2018/Python/Assignments/Assignment#6/hw#6PartII_BryanGreener.py
+++ Spring 2018/Python/Assignments/Assignment#6/hw#6PartII_BryanGreener.py
@@ -3,17 +3,18 @@
 # Homework: 6 part 2
 
 import sys
+import csv
 
 def MemoizeReset(f):
     cache = {} # Dict acting as cache
     def MR(*args, **kwargs): # Main helper func. Accumulates count and caches
         val = str(args) + str(kwargs)
         if val not in cache:
-            MemoizeReset.count += 1
             cache[val] = f(*args, **kwargs)
+            MR.count += 1
         return cache[val]
-    MR.count = 0
     MR.__name__ = f.__name__
+    MR.count = 0
     return MR
 
 @MemoizeReset # Decorator for func above
@@ -31,23 +32,25 @@ def lev(a, b):
                   lev(a[:-1], b[:-1]) + cost])
     return output
 
-import csv
-if sys.argv[1] == "-f" and len(sys.argv) > 2:
-    filename = sys.argv[2]
-else:
+# If no input arg specified
+if len(sys.argv) < 2:
     print("No file specified. Exiting")
     exit()
+else: #input arg specified. save to filename
+    filename = sys.argv[1]
 try:
     with open(filename, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
+            count = lev.count
+            lev.count = 0 #reset counter in decorator
             print("%s,%s,%d,%s" % (row[0].strip(), 
                                    row[1].strip(), 
                                    lev(row[0].strip(),row[1].strip()),
-                                   str(lev.count)))
-except OSError as err:
+                                   str(count)))
+except OSError as err: # Invalid input file name
     print("OS Error: {0}".format(err))
     exit()
-except NameError as err:
-    print("Type Error: {0}".format(err))
+except (ValueError,IndexError) as err: #Invalid input file content
+    print("Input Error: {0}".format(err))
     exit()

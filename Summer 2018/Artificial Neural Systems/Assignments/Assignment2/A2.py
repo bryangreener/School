a5b3bef80bd2325d1abd11ea7d0eca69c2d699af
Bryan Greener <bryangreener@users.noreply.github.com>
2018-05-21 11:52:08 -0400

    Update A2.py
    Update a2 with new structure for backward method.

--- Summer 2018/Artificial Neural Systems/Assignments/Assignment2/A2.py
+++ Summer 2018/Artificial Neural Systems/Assignments/Assignment2/A2.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun May 20 10:03:36 2018
-
 @author: Bryan
 """
 
 import numpy as np
 import pandas as pd
-import csv
 import random
 
 class Network():
     def __init__(self, layers):
         self.layers = layers
-        self.w = [np.random.randn(y,x) for x,y in zip(layers[:-1],layers[1:])]
-        self.a = [np.random.randn(x,1) for x in self.layers]
-        self.z = [np.random.randn(x,1) for x in self.layers]
+        self.w = [np.random.randn(x,y) for x,y in zip(layers[:-1],layers[1:])]
+        self.a = [np.random.randn(1,x) for x in self.layers]
+        self.z = [np.random.randn(1,x) for x in self.layers]
 
     def sigmoid(self, z):
         return 1.0/(1.0+np.exp(-z))
@@ -24,30 +22,27 @@ class Network():
         return np.exp(-z)/((1.0 + np.exp(-z))**2)
 
     def forward(self, x):
-        self.a[0] = x
+        # Preload input layer activation with inputs
+        self.a[0] = self.sigmoid(x)
         for i in range(1, len(self.layers)):
-            self.a[i] = self.sigmoid(np.dot(self.a[i-1], self.w[i-1].T))
+            self.z[i] = np.dot(self.a[i-1], self.w[i-1])
+            self.a[i] = self.sigmoid(self.z[i])
         return self.a[-1]
 
     def backward(self, x, y, eta):
-        #### Calculate error of last layer
-            self.z[-1] = self.sigmoid(np.dot(self.a[-2], self.w[-1].T))
-            delta = np.multiply(-(y-self.a[-1], self.sigmoidPrime(self.z[-1])))
-            self.w[-1] = self.w[-1] - eta * delta
-            # Normalize
-            self.w[-1] = self.w[-1] / self.w[-1].max(axis=0)
-            
-        #### Send errors backward
-            for i in range(len(self.layers) - 2, 0, -1):
-                self.z[i] = self.sigmoid(np.dot(self.a[i-1], self.w[i-1].T))
-                delta = np.multiply(-(y-self.a[-1]),
-                                    self.sigmoidPrime(self.z[i-1]))
-                dw = np.divide(np.dot(self.a[i-1].T, delta) + self.w[i].T, 
-                               x.shape[0])
-                self.w[i] = self.w[i] - eta * dw
-                # Normalize
-                self.w[i] = self.w[i] / self.w[i].max(axis=0)
-                
+        lam = 1e-4
+        self.a[0] = x #update activation at first layer with inputs
+        #### Calculate partial derivatives of cost (cost prime)
+        for i in range(len(self.layers)-2, 0, -1):
+            if i == len(self.layers)-2: #y-yhat for last layer
+                delta = np.dot((self.a[-1]-y), self.sigmoidPrime(self.z[-1]))
+            else: 
+                delta = np.dot(delta,self.w[i+1].T)*self.sigmoidPrime(self.z[i+1])
+            dw = np.divide((np.dot(delta.T,self.a[i]).T + lam * self.w[i]), 
+                           (x.shape[0]))
+            self.w[i] = self.w[i] - eta * dw
+            self.w[i] = self.w[i] / self.w[i].max(axis=0)
+        
     def train(self, trainData, testData, epochs, eta):
         for epoch in range(epochs):
             random.shuffle(trainData)
@@ -59,8 +54,8 @@ class Network():
             accuracy = 0.0
             for t in testData:
                 accuracy += (t[-1] - self.forward(t[:-1]))
-            print("Epoch {0},{1}  {2}% accuracy".Format(epoch, epochs,
-                  (accuracy/testData.count)*100))
+            print("Epoch {0}/{1}  {2}% accuracy".format(epoch, epochs,
+                  (accuracy/testData.shape[0])*100))
             
 def category(df):
     for col in list(df.select_dtypes(exclude=[np.number])):
@@ -79,4 +74,3 @@ testData = inputData[round(inputData.shape[0] * 0.75 + 1):]
 
 net = Network([inputData.shape[1]-1, 10, 20, 1])
 net.train(trainData, testData, 1000, 0.01)
-
